fce348c2ef90fb254c069bade6691fab8b1cea49
jakub-galecki <324384@uwr.edu.pl>
2023-01-27 00:38:34 +0100

    feat: parse request to logical plan

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,6 +1,9 @@
 module Json = Yojson.Basic
 module JsonUtil = Yojson.Basic.Util
 
+exception Error of string
+
+
 (*
 JSON REQUEST
 {
@@ -21,14 +24,14 @@ If alias doesnt exists it equals to the table name
 
 (* TODO : ALL OF THIS SHIT WORKS ONLY FOR ONE ELEM *)
 
-type alias =  string * string
-type property = string * string
+type alias =  string * string [@@deriving show] 
+type property = string * string [@@deriving show]
 
 type tpredicate = {
   x: property; 
   op: string;
   y: property;
-}
+} [@@deriving show]
 
 (* tpredicate TableName.property operator TableName.property *)
 let is_tpredicate str = 
@@ -37,7 +40,8 @@ let is_tpredicate str =
 
 let tpredicate_from_string str = 
   let split_by_dot = Str.split (Str.regexp "\\.") in
-  match Str.split (Str.regexp " ") str with 
+  let splitted_pred = Str.split (Str.regexp " ") str in
+  match splitted_pred with 
   | x :: op :: y :: _ -> 
     begin
     let a1 = split_by_dot x and a2 = split_by_dot y in 
@@ -46,16 +50,16 @@ let tpredicate_from_string str =
         begin 
         {x = (tname1,property1); op= op; y= (tname2, property2)}
         end
-      | _ -> failwith "Wrong predicate"
+      | _ -> raise (Error  "52: Wrong predicate")
     end
-  | _ -> failwith "Wrong predicate"
+  | _ -> raise (Error  "54: Wrong predicate")
 
 
 type cpredicate = {
   x: property; 
   op: string;
   y: string;
-}
+} [@@deriving show]
 
 let is_cpredicate str =  
     let regx = Str.regexp "[a-zA-z1-9]+\\.[a-zA-z1-9]+ . [a-zA-Z1-9]+" in
@@ -73,32 +77,59 @@ let cpredicate_from_string str =
         begin 
         {x = (tname1,property1); op= op; y= const }
         end
-      | _ -> failwith "Wrong predicate"
+      | _ -> raise (Error  "79: Wrong predicate")
     end
-  | _ -> failwith "Wrong predicate"
+  | _ -> raise (Error  "81: Wrong predicate")
 
-type method_ = string
-type tuples = (string * string) list
-type froms = alias list
-type joins = (string * tpredicate) list
-type tpredicates = tpredicate list
-type cpredicates = cpredicate list
+type method_ = string [@@deriving show]
+type tuples = (string * string) list [@@deriving show]
+type froms = alias list [@@deriving show]
+type joins = (string * tpredicate) list [@@deriving show]
+type tpredicates = tpredicate list [@@deriving show]
+type cpredicates = cpredicate list [@@deriving show]
 
-(* type logicalPlan = {
-  met: method_ ref;
-  tups: tuples ref;
-  fms: froms ref;
-  jns: joins ref;
-  tps : tpredicates ref;
-  cps : cpredicates ref;
-} *)
+type logical_plan = {
+  met: method_ ;
+  tups: tuples ;
+  fms: froms ;
+  jns: joins ;
+  tps : tpredicates;
+  cps : cpredicates;
+} [@@deriving show]
 
+(* Implement custom formatters *)
+
+let print_tuples (tps: tuples) =
+  let pair_printer = Fmt.(pair ~sep:comma string string) in 
+  let list_printer = Fmt.(list ~sep:(const pf ";@ ") pair_printer) in 
+  Logs.info(fun m -> m "TUPLES: %a" list_printer tps) 
+
+
+let print_froms (fms: froms) = 
+  let pair_printer = Fmt.(pair ~sep:comma string string) in 
+  let list_printer = Fmt.(list ~sep:(const pf ";@ ") pair_printer) in 
+  Logs.info(fun m -> m "FROMS: %a" list_printer fms)  
+
+
+let print_joins  (jns : joins) =
+  let pair_printer = Fmt.(pair ~sep:comma string pp_tpredicate) in 
+  let list_printer = Fmt.(list ~sep:(const pf ";@ ") pair_printer) in 
+  Logs.info(fun m -> m "JOINS: %a" list_printer jns)  
+
+let print_tpredicates (tps: tpredicates) = 
+  let list_printer = Fmt.(list ~sep:(const pf ";@ ") pp_tpredicate) in 
+  Logs.info(fun m -> m "TABLE PREDICATES: %a" list_printer tps)  
+ 
+let print_cpredicated (cps: cpredicates) = 
+  let list_printer = Fmt.(list ~sep:(const pf ";@ ") pp_cpredicate) in 
+  Logs.info(fun m -> m "TABLE PREDICATES: %a" list_printer cps)  
+ 
 (* let is_table_alias str = 
   let rgx = Str.regexp "^[a-aA-Z]*\\([a-aA-Z]+\\)" in 
   Str.string_match rgx str 0 *)
   
 let print_json (json :Json.t) = 
-  Logs.info( fun m -> m "Parsed json: %a" Json.pp json)
+  Logs.info(fun m -> m "Parsed json: %a" Json.pp json)
 
 (* Wraper todo docs *)
 let try_retrive member json =
@@ -119,13 +150,13 @@ let members = [
 let remove_last_char str = 
   let n = String.length str in 
     if n = 0 then 
-      failwith "Can not remove char from empty string"
+      raise (Error  "Can not remove char from empty string")
     else String.sub str 0 (n - 1) 
 
 
 let handle_method json : method_ = 
   match try_retrive "method" json with
-  | None -> failwith "Method not specified"
+  | None -> raise (Error  "Method not specified")
   | Some(m) -> m |> JsonUtil.to_string
 
 
@@ -134,7 +165,7 @@ let handle_tuples json : tuples =
     let rgx = Str.regexp "^.+\\..+$" in 
     Str.string_match rgx str 0 in
   match try_retrive "tuples" json with 
-  | None -> failwith "Tuples not specified"
+  | None -> raise (Error  "Tuples not specified")
   | Some(arr_t) -> 
     (* todo make this better *)
     let arr = JsonUtil.filter_string (JsonUtil.to_list arr_t) in
@@ -142,65 +173,63 @@ let handle_tuples json : tuples =
       let split_str = Str.split (Str.regexp "\\.") in
       let helper str = 
         let splitted_arr = split_str str in 
-          if List.length splitted_arr < 2 then failwith "Wrong tuple definition"
+          if List.length splitted_arr < 2 then raise (Error  "Wrong tuple definition")
           else ((List.nth splitted_arr 0), (List.nth splitted_arr 1)) in
         List.map helper filtered_arr
 
 
 let handle_froms json : froms =
   let is_from str =
-    let rgx = Str.regexp "^[a-aA-Z]*\\([a-aA-Z]+\\)" in 
+    let rgx = Str.regexp "^[a-zA-Z]*([a-zA-Z]+)" in 
     Str.string_match rgx str 0 in 
   match try_retrive "froms" json with 
-  | None -> failwith "Froms not specified"
+  | None -> raise (Error  "Froms not specified")
   | Some(arr_f) -> 
     let arr = JsonUtil.filter_string (JsonUtil.to_list arr_f) in
     let filtered_arr = List.filter is_from arr in 
     (* This should have following form : ["table name"; "alias>"] *)
-    let split_str = Str.split (Str.regexp "\\(") in
+    let split_str = Str.split (Str.regexp "(") in
     let helper str = 
       let splitted = split_str str in 
-        if List.length splitted < 2 then failwith "Wrong from definition"
+        if List.length splitted < 2 then raise (Error  "Wrong from definition")
         else 
           let tname = (List.nth splitted 0) and a = (List.nth splitted 1) in 
-          let n = String.length a in if n = 0 then failwith "Wrong alias" else 
+          let n = String.length a in if n = 0 then raise (Error  "Wrong alias") else 
             (tname, remove_last_char a) in 
     List.map helper filtered_arr
 
 
-let handle_joins json : (string * cpredicate) list * (string * tpredicate) list =
+let handle_joins json : (string * tpredicate) list =
   (*  joins: [alias<predicate>, ...] *)
-  let is_from str =
-    let rgx = Str.regexp "^[a-aA-Z]*\\<[a-aA-Z]+\\>" in 
-    Str.string_match rgx str 0 in 
-  match try_retrive "froms" json with 
-  | None -> failwith "Froms not specified"
+  (* let is_join str =
+    let rgx = Str.regexp "^[a-zA-Z]*<[a-zA-Z]+>" in 
+    Str.string_match rgx str 0 in  *)
+  match try_retrive "joins" json with 
+  | None -> raise (Error  "Froms not specified")
   | Some(arr_f) -> 
     let arr = JsonUtil.filter_string (JsonUtil.to_list arr_f) in
-    let filtered_arr = List.filter is_from arr in 
+    (* let filtered_arr = List.filter (fun s ->
+      let _s = remove_last_char s in
+      if (is_join _s) || (is_tpredicate _s) then true else false  
+    ) arr in  *)
+    (* Logs.info(fun m -> m "Filtered array for joins: %a" (Fmt.list Fmt.string) filtered_arr); *)
     (* This should have following form : ["table name"; "alias>"] *)
-    let split_str = Str.split (Str.regexp "\\(") in
+    let split_str = Str.split (Str.regexp "<") in
     let helper str = 
       let splitted = split_str str in 
-        if List.length splitted < 2 then failwith "Wrong join definition"
+        if List.length splitted < 2 then raise (Error  "Wrong join definition")
         else 
           let tname = (List.nth splitted 0) and p = (List.nth splitted 1) in 
-          let n = String.length p in if n = 0 then failwith "Wrong predicate" else 
+          let n = String.length p in if n = 0 then raise (Error  "191: Wrong predicate") else 
             let predicate = remove_last_char p in 
-              if is_cpredicate predicate then 
-                Either.left(tname, (cpredicate_from_string predicate))
-              else Either.right(tname, (tpredicate_from_string predicate)) in
-    let r = List.map helper filtered_arr in
-     List.fold_left (fun acc elem -> 
-        match elem with 
-        | Either.Left(p) -> ((List.append (fst acc) [p]), (snd acc))
-        | Either.Right(p) -> ((fst acc), (List.append (snd acc) [p]))
-      ) ([], []) r
-
+              if is_tpredicate predicate then 
+                (tname, (tpredicate_from_string predicate))
+              else raise (Error "Only tpredicates are allowed for joins") in 
+    List.map helper arr
 
 let handle_predicates json : (cpredicates * tpredicates) = 
-  match try_retrive "froms" json with 
-  | None -> failwith "Froms not specified"
+  match try_retrive "predicates" json with 
+  | None -> raise (Error  "Froms not specified")
   | Some(arr_f) -> 
     let arr = JsonUtil.filter_string (JsonUtil.to_list arr_f) in 
     List.fold_left (fun acc elem -> 
@@ -210,12 +239,17 @@ let handle_predicates json : (cpredicates * tpredicates) =
     ) ([], []) arr 
 
 
+(* TODO: Create method to create LOGICAL PLAN *)
 let parse_to_logical_plan json = print_json json;
   let m = handle_method json 
-  and _ = handle_tuples json 
-  and _ = handle_froms json 
-  and _ = handle_joins json
-  and _ = handle_predicates json in 
-  print_endline m;
+  and t = handle_tuples json 
+  and fms = handle_froms json 
+  and jns = handle_joins json
+  and preds = handle_predicates json in 
+  let lp = {met=m;tups=t;fms=fms;jns=jns;cps=(fst preds);tps=(snd preds)} in 
+  Logs.info(fun m -> m "LP: %a" pp_logical_plan lp)
+
+  
+
 
 
